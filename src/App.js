bf4815cd98be830f559702c68c9cabb66cb5e700
ironsocks <brach.thomasja@gmail.com>
2020-12-10 11:25:34 +0100

    homepage broken down, styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import "./App.css";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
