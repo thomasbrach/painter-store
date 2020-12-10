e1f5b487c598ce651e34e06facbb2c98a46eb568
ironsocks <brach.thomasja@gmail.com>
2020-12-10 12:25:00 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <Fragment>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="shop/hats" />
+      </Switch>
     </Fragment>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -42,8 +42,8 @@ const Directory = () => {
 
   return (
     <div className="directory-menu">
-      {sections.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      {sections.map(({ id, ...sectionProps }) => (
+        <MenuItem key={id} {...sectionProps} />
       ))}
     </div>
   );
