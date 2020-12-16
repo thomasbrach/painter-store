2f37358320687f5f53368dd42331d278999da6b2
ironsocks <brach.thomasja@gmail.com>
2020-12-16 12:08:45 +0100

    added redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import persistStore from "redux-persist/es/persistStore";
 import thunk from "redux-thunk";
@@ -11,6 +12,9 @@ if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 export const persistor = persistStore(store);
