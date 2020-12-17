d5148083e776d089a1b6544964dad300bbbd78e9
ironsocks <brach.thomasja@gmail.com>
2020-12-17 16:56:58 +0100

    moved frontend to client folder, added express server for stripe integration

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,21 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51HyaVzJdjbbnF1GvNJ1n9c8pv8zS7vkZWJfFU3B4OBJmxfhMR0l1c7FE4WKUqBRV8iQcRaazmXBi2nYvCCgvRcdT00wgnp9qxE";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment successful!");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => alert("Payment successful!"))
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
